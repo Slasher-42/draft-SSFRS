174dbaea1544ae8bcefa9a1fffb2629b7aa2715d
ngabonzizacedrickkennedy <cedzlankee42@gmail.com>
2026-04-15 20:14:57 +0200

    first commit

---
+++ backend/ProjectWorker-Execution-Service/src/main/java/com/example/ProjectWorker_Execution_Service/ProjectWorkerExecutionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.ProjectWorker_Execution_Service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectWorkerExecutionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectWorkerExecutionServiceApplication.class, args);
+	}
+
+}
