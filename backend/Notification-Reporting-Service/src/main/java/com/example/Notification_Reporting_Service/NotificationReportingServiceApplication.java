87734ddcdd6b292f1be5d5cfd96e33d17a0ef805
ngabonzizacedrickkennedy <cedzlankee42@gmail.com>
2026-05-25 14:28:28 +0200

    scratch

---
+++ backend/Notification-Reporting-Service/src/main/java/com/example/Notification_Reporting_Service/NotificationReportingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.Notification_Reporting_Service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationReportingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationReportingServiceApplication.class, args);
+	}
+
+}
